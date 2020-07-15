bd6fed3aa9fab7f4b0e8894919b64e8e27f51f39
Jannis Backhaus <43847206+JannisBackhaus@users.noreply.github.com>
2020-07-16 00:04:59 +0200

    snap update

--- src/js/main.js
+++ src/js/main.js
@@ -184,11 +184,11 @@ function moveStep(_bar, _step, _debug) {
 
 
 
-//var bar1 = new Bar(centerX, centerY, 0);
-//bar1
+var bar1 = new Bar(centerX, centerY, 0);
+bar1.all.drag(bar1.move1, bar1.start, bar1.stop1)
 
 
-/*
+
 var bar2 = new Bar(centerX, centerY, 45);
 bar2.all.drag(bar2.move2, bar2.start, bar2.stop);
 
@@ -197,7 +197,7 @@ bar3.all.drag(bar3.move3, bar3.start, bar3.stop);
 
 var bar4 = new Bar(centerX, centerY, 135);
 bar4.all.drag(bar4.move4, bar4.start, bar4.stop);
-*/
+
 
 
 
